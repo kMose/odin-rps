20f51373d16ef49c7ab0a89ea5e760ee1a998cd7
kMose <bennegron62@gmail.com>
2022-07-18 13:03:14 -0400

    Add hand images and edit center text announcement

--- scripts.js
+++ scripts.js
@@ -23,13 +23,13 @@ function determineWinner(){
     
     // Figures out the winner.
        if (playerScore == 5){
-            alert("You WIN. YOU BEAT COMPUTER. HOW IS THIS EVEN POSSIBLE?");
+            gameOutput.innerText = "You WIN. YOU BEAT COMPUTER. HOW IS THIS EVEN POSSIBLE?";
             playerScore = 0;
             computerScore = 0;
         }
 
         if (computerScore == 5){
-            alert("YOU LOSE! GOOD DAY SIR.");
+            gameOutput.innerText = "YOU LOSE! GOOD DAY SIR.";
             playerScore = 0;
             computerScore = 0;
         }
@@ -43,12 +43,14 @@ function playRound(userInput, computerInput){
     if (userInput == "rock"){
         if (computerInput == "paper"){computerInput
             gameOutput.innerText = `You LOSE! ${computerInput.toUpperCase()} beats ${userInput.toUpperCase()}! `;
+            outcomeText.innerText = "YOU LOSE!"
             computerScore++;
             determineWinner();
             return 0;
         }
         if (computerInput == "scissors"){
             gameOutput.innerText = `You WIN! ${userInput.toUpperCase()} beats ${computerInput.toUpperCase()}! `;
+            outcomeText.innerText = "YOU WIN!"
             playerScore++;
             determineWinner();
             return 0;
@@ -58,12 +60,15 @@ function playRound(userInput, computerInput){
     if (userInput == "paper"){
         if (computerInput == "scissors"){
             gameOutput.innerText = `You LOSE! ${computerInput.toUpperCase()} beats ${userInput.toUpperCase()}! `;
+            outcomeText.innerText = "YOU LOSE!"
             computerScore++;
             determineWinner();
             return 0;
         }
         if (computerInput == "rock"){
             gameOutput.innerText = `You WIN! ${userInput.toUpperCase()} beats ${computerInput.toUpperCase()}! `;
+            outcomeText.innerText = "YOU WIN!"
+            
             playerScore++;
             determineWinner();
             return 0;
@@ -73,12 +78,14 @@ function playRound(userInput, computerInput){
     if (userInput == "scissors"){
         if (computerInput == "rock"){
             gameOutput.innerText = `You LOSE! ${computerInput.toUpperCase()} beats ${userInput.toUpperCase()}! `;
+            outcomeText.innerText = "YOU LOSE!"
             computerScore++;
             determineWinner();
             return 0;
         }
         if (computerInput == "paper"){
             gameOutput.innerText = `You WIN! ${userInput.toUpperCase()} beats ${computerInput.toUpperCase()}! `;
+            outcomeText.innerText = "YOU WIN!"
             playerScore++;
             determineWinner();
             return 0;
@@ -87,6 +94,7 @@ function playRound(userInput, computerInput){
 
     if (userInput == computerInput){
         gameOutput.innerText = "It's a tie!";
+        outcomeText.innerText = "TIE!"
         return 0;
     }
 
@@ -95,6 +103,7 @@ function playRound(userInput, computerInput){
 
 }
 
+let outcomeText = document.querySelector(".outcome-text");
 let rockButton = document.querySelector(".rock");
 let paperButton = document.querySelector(".paper");
 let scissorsButton = document.querySelector(".scissors");
